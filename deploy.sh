da4fd1fae456b0916cc1317598641977ebfeb299
Sandeep Jadoonanan <jsanweb@gmail.com>
2018-10-29 21:10:08 -0400

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Get absolute path of the script.
+PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
+ROOT_PATH=$( cd "$(dirname "${PARENT_PATH}")" ; pwd -P )   
+
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
+BUCKET="s3://sandeep.sh"
+BUILD_DIR="$PARENT_PATH/build"
+
+BUILD_CMD="mkdir -p build && cp index.html build/ && cp sandeepfavicon.ico build/ && cp -R css build/ && cp -R img build/"
+CLEAR_CMD="aws s3 rm $BUCKET --recursive"
+UPLOAD_CMD="aws s3 sync $BUILD_DIR $BUCKET"
+
+# -------------------------------------------------------------------
+# -------------------------------------------------------------------
+
+echo -e "\n-------$YELLOW Building for Production $NC-------"
+echo -e $BUILD_CMD
+$BUILD_CMD
+
+echo -e "\n-------$YELLOW Clearing S3 Bucket $NC-------"
+echo -e $CLEAR_CMD
+$CLEAR_CMD
+
+echo -e "\n-------$YELLOW Uploading to S3 Bucket $NC-------"
+echo -e $UPLOAD_CMD
+$UPLOAD_CMD
